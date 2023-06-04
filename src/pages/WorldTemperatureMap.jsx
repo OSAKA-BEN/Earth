57f463337579e8e35c45b1c224bade3fee9fb1f8
BENJAMIN GUIGANTON <benjamin.guiganton@gmail.com>
2023-06-04 22:29:37 +0900

    Supression de l'échelle température, modification couleur

--- src/pages/WorldTemperatureMap.jsx
+++ src/pages/WorldTemperatureMap.jsx
@@ -9,118 +9,86 @@ function WorldTemperatureMap() {
     const width = 800, height = 800;
 
     const projection = d3.geoOrthographic()
-        .scale(350)
-        .translate([width / 2, height / 2])
-        .clipAngle(90) 
-        .precision(.1)
-        .rotate([0,0,0]);
+      .scale(350)
+      .translate([width / 2, height / 2])
+      .clipAngle(90)
+      .precision(.1)
+      .rotate([0, 0, 0]);
 
     const path = d3.geoPath()
-        .projection(projection);
+      .projection(projection);
 
     const svg = d3.select(ref.current)
-        .attr("width", width)
-        .attr("height", height);
+      .attr("width", width)
+      .attr("height", height);
 
     // Append all meridians and parallels
     const graticule = d3.geoGraticule();
     svg.append("path")
-        .datum(graticule)
-        .attr("class", "graticule")
+      .datum(graticule)
+      .attr("class", "graticule")
+      .attr("d", path);
+
+    d3.json("/world-countries.json").then(function (collection) {
+		svg.selectAll("path")
+		.data(collection.features.sort((a, b) => a.properties.name.localeCompare(b.properties.name)))
+		.enter().append("a")
+        .attr("xlink:href", d => "https://www.google.com/search?q=" + d.properties.name)
+        .append("path")
         .attr("d", path)
-    
-		d3.json("/world-countries.json").then(function(collection) {
-			var countries = svg.selectAll("path")
-				.data(collection.features)
-				.enter().append("a")
-				.attr("xlink:href", d => "https://www.google.com/search?q=" + d.properties.name)
-				.append("path")
-				.attr("d", path)
-				.attr("class", "country")
-				.attr("id", d => d.id)
-				.on("mouseover", function(event, d) {
-					d3.select(this).style("fill", "#9966cc");
-					setCountryName(d.properties.name);  // Update the state here
-				})
-				.on("mouseout", function(event, d) {
-					d3.select(this).style("fill", "");
-					setCountryName("");  // Clear the state here
-				});
-			
-		d3.csv("/world-temperature.csv").then(function(data) {
-			// 60 is the number of class in temperature.css
-			var quantile = d3.scaleQuantile().domain([
-				d3.min(data, e => e.temperature),
-				d3.max(data, e => +e.temperature)])
-				.range(d3.range(60));
-				
-			var legend = svg.append('g')
-				.attr('transform', 'translate(35, 10)')
-				.attr('id', 'legend');
-				
-			legend.selectAll('.colorbar')
-				.data(d3.range(60))
-				.enter().append('rect')
-				.attr('y', d => d * 5 + 'px')
-				.attr('height', '5px')
-				.attr('width', '20px')
-				.attr('x', '0px')
-				.attr("class", d => "temperature-" + d);
-			
-			let legendScale = d3.scaleLinear()
-				.domain([d3.min(data, e => +e.temperature), d3.max(data, e => +e.temperature)])
-				.range([0, 60 * 5]);
-				
-			svg.append("g")
-				.attr('transform', 'translate(25, 10)')
-				.call(d3.axisLeft(legendScale).ticks(10));
-			
-			data.forEach(function(e,i) {
-				d3.select("#" + e.country)
-					.attr("class", d => "country temperature-" + quantile(+e.temperature));
-			});
-		});
-	});
-	
-	const lambda = d3.scaleLinear()
-		.domain([0, width])
-		.range([-180, 180]);
+        .attr("class", "country")
+        .attr("id", d => d.id)
+		.attr("fill", "white")
+        .on("mouseover", function (event, d) {
+          d3.select(this).style("fill", "#0ff");
+          setCountryName(d.properties.name);  // Update the state here
+        })
+        .on("mouseout", function (event, d) {
+          d3.select(this).style("fill", "");
+          setCountryName("");  // Clear the state here
+        });
+
+    });
+
+    const lambda = d3.scaleLinear()
+      .domain([0, width])
+      .range([-180, 180]);
 
     const phi = d3.scaleLinear()
-		.domain([0, height])
-		.range([90, -90]);
+      .domain([0, height])
+      .range([90, -90]);
 
-    var drag = d3.drag().subject(function() {
-        var r = projection.rotate();
-        return {
-			x: lambda.invert(r[0]),
-			y: phi.invert(r[1])
-		};
-	}).on("drag", function(event) {
-		projection.rotate([lambda(event.x), phi(event.y)]);
+    var drag = d3.drag().subject(function () {
+      var r = projection.rotate();
+      return {
+        x: lambda.invert(r[0]),
+        y: phi.invert(r[1])
+      };
+    }).on("drag", function (event) {
+      projection.rotate([lambda(event.x), phi(event.y)]);
 
-		svg.selectAll(".graticule")
-			.datum(graticule)
-			.attr("d", path);
-		
-		svg.selectAll(".country")
-			.attr("d", path);
-	});
-	
-	svg.call(drag);
+      svg.selectAll(".graticule")
+        .datum(graticule)
+        .attr("d", path);
+
+      svg.selectAll(".country")
+        .attr("d", path);
+    });
+
+    svg.call(drag);
 
   }, []);
 
   return (
-	<div className='p-4 z-[1]'>
-	<h1 className="alien-title text-center font-extrabold text-3xl tracking-wider shadow-neon">
-		{countryName || 'Hover over a country'}
-	</h1>
-	<h2 className="text-center text-2xl font-bold ">
-		{countryName || 'Hover over a country'}
-	</h2>
-	<svg ref={ref}></svg>
-	</div>
+    <div className='p-4 z-[1]'>
+      <h1 className="alien-title text-center font-extrabold text-3xl tracking-wider shadow-neon">
+        {countryName || 'Hover over a country'}
+      </h1>
+      <h2 className="text-center text-2xl font-bold">
+        {countryName || 'Hover over a country'}
+      </h2>
+      <svg ref={ref}></svg>
+    </div>
   );
 }
 
